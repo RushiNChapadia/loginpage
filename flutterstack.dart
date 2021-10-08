df8e82756c78bee9ff4f1a019da06f3ff5c43fc3
rambo863 <72151291+rambo863@users.noreply.github.com>
2021-10-08 16:56:58 +0530

    Create flutterstack.dart

---
+++ flutterstack.dart
@@ -0,0 +1,14 @@
+Stack(  
+  children: <Widget>[  
+    // Max Size  
+    Container(  
+      color: Colors.green,  
+    ),  
+    Container(  
+      color: Colors.blue,  
+    ),  
+    Container(  
+      color: Colors.yellow,  
+    )  
+  ],  
+),  
