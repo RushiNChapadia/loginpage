536c36359d66e61ad7b50194c9a2e1d80f18ec75
Akash Lilhare <akashlilhare14@gmail.com>
2021-10-22 19:05:47 +0530

    custom button

---
+++ custom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:portfoli/constants/app_theme.dart';
+import 'package:provider/provider.dart';
+
+class CustomButton extends StatelessWidget {
+  final Function onTap;
+  final String title;
+
+  const CustomButton({Key? key,required this.onTap,required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDark = Provider.of<ThemeProvider>(context).isDarkMode;
+    return    ElevatedButton(
+      style: ElevatedButton.styleFrom(
+primary: isDark ? AppTheme.darkButtonColor : AppTheme.lightButtonColor,
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0)),
+        minimumSize: const Size(150, 55),
+        maximumSize: const Size(200, 55),
+      ),
+      onPressed: () {onTap();},
+      child:
+          Text(title,style: const TextStyle(color: Colors.white),),
+
+
+    );
+  }
+}
