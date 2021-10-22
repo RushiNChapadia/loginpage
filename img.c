c80dd647ecbc861a83917f6e7931eabfb7596ea3
VASU068 <91270312+VASU068@users.noreply.github.com>
2021-10-22 18:09:16 +0530

    Create img.c

---
+++ img.c
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+img {
+  width: 100%;
+}
+</style>
+</head>
+<body>
+
+<img src="html5.gif" alt="HTML5 Icon" width="128" height="128">
+
+<img src="html5.gif" alt="HTML5 Icon" style="width:128px;height:128px;">
+
+</body>
+</html>
