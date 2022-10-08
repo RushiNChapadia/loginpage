23c1e4888c31a11d472478965dc32695c7c9a222
rakpatel808 <72158796+rakpatel808@users.noreply.github.com>
2022-10-08 15:07:09 +0530

    Update flutterstack.dart

--- flutterstack.dart
+++ flutterstack.dart
@@ -15,3 +15,18 @@ Stack(
     ),
   ],  
 ),  
+ PUSHING ELEMENT IN STACK
+   
+   kStack<int> intStack = Stack();
+
+// pushing items into the stack.
+intStack.push(3);
+intStack.push(4);
+intStack.push(5);
+
+// printing the values of the items in the stack
+intStack.print();
+
+  // 5
+  // 4
+  // 3
